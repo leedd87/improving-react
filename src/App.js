7f85c4e214f163c7d937ed4752ca093953091190
leedd87 <leedav87@gmail.com>
2022-02-24 23:17:42 +0900

    pushing to repository gitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Componente from "./components/Component";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<section>
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Hello Universo</p>
+				</section>
+				<section>
+					<Componente msg="Hi Im a component from a prop expresado" />
+					<hr />
+					<Propiedades
+						cadena="esto es una cadenad de texto"
+						numero={19}
+						booleano={true}
+						arreglo={[1, 2, 3, 4, 5]}
+						objeto={{ nombre: "David", correo: "micorreo@correo.com" }}
+						elementoReact={<i>Esto es un elemento React</i>}
+						funcion={(num) => num * num}
+						componenteReact={
+							<Componente msg="soy un componente pasado como Prop" />
+						}
+					/>
+					<hr />
+					<Estado />
+				</section>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// class Componente extends Component {
+// 	render() {
+// 		return <h2>{this.props.msg}</h2>;
+// 	}
+// }
+
+// function Componente(prop) {
+// 	return <h2>{prop.msg}</h2>;
+// }
+
+const Componente = (props) => <h2>{props.msg}</h2>;
+
+export default Componente;
